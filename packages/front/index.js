6b0183ca9d1f369f8b3fcd5cfdf23baa31f89156
AugustoPeralta <augustoperaltamontero@gmail.com>
2021-04-05 18:23:42 -0300

    Update index.js

--- packages/front/index.js
+++ packages/front/index.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-  // TODO: say hi, again
+  // TODO: say hi, again and again
   console.log("BEHOLD MY FRONTEND PACKAGE!");
     console.log("hello all, from @gskTest/common");
     console.log("hello from @gskTest/common again");

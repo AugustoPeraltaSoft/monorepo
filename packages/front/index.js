d740566f336bef7d2299fadece35b5e30bb9efb9
AugustoPeralta <augusto.peralta@softvision.com>
2021-04-07 13:20:58 -0300

    feat: adding zipping

--- packages/front/index.js
+++ packages/front/index.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   // TODO: say hi, again and again and more
-  console.log("BEHOLD MY FRONTEND PACKAGE!");
+    console.log("BEHOLD MY FRONTEND PACKAGE!");
     console.log("hello peoplee, from @gskTest/common");
     console.log("hello from @gskTest/common again");
 };
